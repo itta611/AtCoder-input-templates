dfcf3f2869475e93056779c806955fbf9e8c0aa3
Itta <86896657+itta611@users.noreply.github.com>
2022-03-27 19:59:19 +0900

    setup next.js and chakra-ui

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import { Container, Heading } from '@chakra-ui/react';
+import Head from 'next/head';
+
+function Index() {
+  return (
+    <Container maxW={'xl'}>
+      <Head>
+        <title>Atcoder input generator</title>
+      </Head>
+      <Heading as={'h2'} size={'2xl'} mt={14}>
+        AtCoder input generator
+      </Heading>
+    </Container>
+  );
+}
+
+export default Index;
