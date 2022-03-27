293e1d918f479c3fe83e343ca9a47e09d106db69
Itta <86896657+itta611@users.noreply.github.com>
2022-03-28 08:20:33 +0900

    add gradiant to title

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,17 @@ function Index() {
       <Head>
         <title>AtCoder input generator</title>
       </Head>
-      <Heading as={'h2'} size={'2xl'} mt={24}>
+      <Heading
+        as={'h2'}
+        size={'2xl'}
+        mt={24}
+        background={'-webkit-linear-gradient(0deg, #05a7ff, #ff05f7)'}
+        backgroundClip={'text'}
+        pb={1}
+      >
         AtCoder input generator
       </Heading>
-      <Text fontSize={'2xl'} mt={7}>
+      <Text fontSize={'2xl'} mt={7} textAlign={'center'}>
         Generate input source code automaticly.
         <br />
         Paste "Input" statement to textbox to generate.
