71c827df1d945d56e28e0001886e4f09777222c9
Itta <86896657+itta611@users.noreply.github.com>
2022-03-28 08:10:21 +0900

    add switch

--- components/InputArea.js
+++ components/InputArea.js
@@ -1,7 +1,24 @@
-import { Textarea } from '@chakra-ui/react';
+import {
+  FormControl,
+  FormLabel,
+  Switch,
+  Textarea,
+  HStack,
+} from '@chakra-ui/react';
 
 function InputArea() {
-  return <Textarea resize={'none'} mt={10} h={40} />;
+  return (
+    <FormControl mt={20}>
+      <FormLabel htmlFor={'input-area'}>Input</FormLabel>
+      <Textarea id={'input-area'} resize={'none'} h={40} />
+      <HStack mt={5}>
+        <FormLabel htmlFor={'copy-switch'} mb={0} flex={1}>
+          Copy to clipboard automaticly
+        </FormLabel>
+        <Switch id={'copy-switch'} marginRight={3} />
+      </HStack>
+    </FormControl>
+  );
 }
 
 export default InputArea;
