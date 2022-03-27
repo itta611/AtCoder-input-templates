ee6cc8b7b19093b08c6d5f5ef45442e20e3772cb
Itta <86896657+itta611@users.noreply.github.com>
2022-03-28 07:56:10 +0900

    update container size

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,14 @@ import InputArea from '../components/InputArea';
 
 function Index() {
   return (
-    <Container maxW={'xl'}>
+    <Container maxW={'2xl'} centerContent>
       <Head>
         <title>AtCoder input generator</title>
       </Head>
       <Heading as={'h2'} size={'2xl'} mt={24}>
         AtCoder input generator
       </Heading>
-      <Text fontSize={'2xl'} textAlign={'center'} mt={7}>
+      <Text fontSize={'2xl'} mt={7}>
         Generate input source code automaticly.
         <br />
         Paste "Input" statement to textbox to generate.
